9563b6a91ceb327ee80bdcd68c7ba40bd059fc0e
Alonso-007 <alonso.afs@gmail.com>
2018-11-11 13:44:03 -0200

    Juntando os componentes

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,26 @@
     <div id="app">
         <h1>Problema de Monty Hall</h1>
         <!-- : serve para passar parametro false sem o : ia passar a string false -->
-        <Door number="1" :hasGift="false" />
+        <div class="form">
+            <div v-if="!started">
+                <label for="portsAmount">Quantas portas ?</label>
+                <input type="text" id="portsAmount" size="3" 
+                    v-model.number="portsAmount"/>
+                <!-- o .number seta o item para numero para ser usado no laco for -->
+            </div>
+            <div v-if="!started">
+                <label for="selectedPort">Qual porta é premiada</label>
+                <input type="text" id="selectedPort" size="3" 
+                    v-model.number="selectedPort"/>
+            </div>
+            <button v-if="!started" @click="started = true">Iniciar</button>
+            <button v-if="started" @click="started = false">Reiniciar</button>
+        </div>
+        <div class="doors" v-if="started">
+            <div v-for="i in portsAmount" :key="i">
+                <Door :hasGift="i === selectedPort" :number="i" />
+            </div>
+        </div>
     </div>
 </template>
 
@@ -12,7 +31,14 @@
 
     export default {
         name: "App", //nao e obrigado mas ajuda na hora depurar o fonte
-        components: { Door }
+        components: { Door },
+        data: function() {
+            return {
+                started: false,
+                portsAmount: 3,
+                selectedPort: null
+            }
+        }
     }
 </script>
 
@@ -38,5 +64,24 @@
         background-color: #0004;
         padding: 20px;
         margin-bottom: 60px;
+    }
+
+    .form {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        margin-bottom: 40px;
+    }
+
+    .form, .form input, .form button {
+        margin-bottom: 10px;
+        font-size: 2rem;
+    }
+
+    .doors {
+        align-self: stretch;
+        display: flex;
+        justify-content: space-around;
+        flex-wrap: wrap;
     }
 </style>
